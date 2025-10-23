36cb0564375e5957ecd3665d618a5c08d8e83b10
Rafium-MS <silveriorafaelsantos@gmail.com>
2025-10-23 15:10:31 -0300

    fix: make jwt module global for shared guard

--- app/backend/src/auth/auth.module.ts
+++ app/backend/src/auth/auth.module.ts
@@ -4,5 +4,9 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
-@Module({ imports: [PrismaModule, JwtModule.register({})], providers: [AuthService], controllers: [AuthController] })
+@Module({
+  imports: [PrismaModule, JwtModule.register({ global: true })],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
 export class AuthModule {}
